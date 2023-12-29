b32152edb2831ab2b73cd8ab10e410438e498aee
Prasthuth <myshoppingapp.prasthuth@gmail.com>
2023-12-30 02:06:54 +0530

    Save and dispose the graph
    Implemented two buttons to save and dispose the graph.(local storage)

--- js/math/graph.js
+++ js/math/graph.js
@@ -7,6 +7,16 @@ class Graph {
         this.segments = segments;
     }
 
+    static load(info) {
+        const points = info.points.map((i) => new Point(i.x, i.y));
+        const segments = info.segments.map((i) => new Segment(
+            points.find((p) => p.equals(i.p1)),
+            points.find((p) => p.equals(i.p2))
+        ));
+
+        return new Graph(points, segments);
+    }
+
     addSegment(seg) {
         this.segments.push(seg);
     }
@@ -62,7 +72,7 @@ class Graph {
         this.points.splice(this.points.indexOf(point), 1)
     }
 
-    dispose(){
+    dispose() {
         this.points.length = 0;
         this.segments.length = 0;
     }
